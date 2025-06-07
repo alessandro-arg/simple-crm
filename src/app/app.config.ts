72b5f963763f44bd629c13dd2f094c987e5148aa
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-07 22:34:58 +0200

    paired correctly my firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,37 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environments } from '../environments/environments';
+
+const firebaseConfig = environments.firebaseConfig;
+
+// // environment.example.ts
+// export const environments = {
+//   production: false,
+//   firebaseConfig: {
+//     apiKey: '',
+//     authDomain: '',
+//     projectId: '',
+//     storageBucket: '',
+//     messagingSenderId: '',
+//     appId: '',
+//   },
+// };
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environments = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCXEeyk9umapm--rdZvUa2SjdlYD1b-C88',
+    authDomain: 'simple-crm-4b560.firebaseapp.com',
+    projectId: 'simple-crm-4b560',
+    storageBucket: 'simple-crm-4b560.firebasestorage.app',
+    messagingSenderId: '69985204819',
+    appId: '1:69985204819:web:05df5fa3c609119f3b64a6',
+  },
+};
